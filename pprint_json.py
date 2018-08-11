9e8c6c8d932792f6dde0b68f65750ad53405ef1c
Zed-chi <zed-chi@mail.ru>
2018-08-11 22:34:26 +0300

    Readme fixes, function purenes fixes

--- pprint_json.py
+++ pprint_json.py
@@ -4,17 +4,33 @@ import sys
 
 def load_data(filepath):
     try:
-        with open(filepath, "r", encoding='utf-8') as f:
-            return json.loads(f.read())
+        with open(filepath, "r", encoding="utf-8") as json_file:
+            return json.loads(json_file.read())
     except OSError:
-        print("invalid path")
+        raise OSError
+    except ValueError:
+        raise ValueError
 
 
-def pretty_print_json(json_data):
-    print(json.dumps(json_data, sort_keys=True, indent=4))
+def pretty_print_json(raw_json_content):
+    print(json.dumps(raw_json_content,
+                     sort_keys=True,
+                     indent=4,
+                     ensure_ascii=False))
 
 
-if __name__ == '__main__':
-    path_to_file = sys.argv[1]
-    json_data = load_data(path_to_file)
-    pretty_print_json(json_data)
+def main():
+    try:
+        path_to_file = sys.argv[1]
+        json_data = load_data(path_to_file)
+        pretty_print_json(json_data)
+    except IndexError:
+        print("No path were provided")
+    except OSError:
+        print("File error")
+    except ValueError:
+        print("JSON file is not valid")
+
+
+if __name__ == "__main__":
+    main()

43622eb5de59af365d55d24e14e5ffd438da8866
Zed-chi <zed-chi@mail.ru>
2018-08-11 15:36:38 +0300

    init

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, "r", encoding='utf-8') as f:
+            return json.loads(f.read())
+    except OSError:
+        print("invalid path")
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    path_to_file = sys.argv[1]
+    json_data = load_data(path_to_file)
+    pretty_print_json(json_data)
